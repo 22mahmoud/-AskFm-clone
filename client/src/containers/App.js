bbdb8a6b3ed9af1b92cae19aa317b9af197ed1ce
22mahmoud <mahmouds1997@yahoo.com>
2018-02-09 07:26:32 +0200

    set upt eslint & ant Design

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Layout } from 'antd';
 
-import Home from "./Home";
-import Login from "./Login";
-import SignUp from "./SignUp";
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
 
+const { Header, Content } = Layout;
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route exact path="/login" component={Login} />
-    </Switch>
-  </BrowserRouter>
+  <Layout style={{ height: '100vh' }}>
+    <Header>Header</Header>
+    <Content>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </Content>
+  </Layout>
 );
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ApolloClient, HttpLink, InMemoryCache } from "apollo-client-preset";
-import { ApolloProvider } from "react-apollo";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
+import { ApolloProvider } from 'react-apollo';
 
-import App from "./containers/App";
-import registerServiceWorker from "./registerServiceWorker";
+import 'antd/dist/antd.css';
+
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:3030/graphql" }),
-  cache: new InMemoryCache()
+  link: new HttpLink({ uri: 'http://localhost:3030/graphql' }),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 registerServiceWorker();
