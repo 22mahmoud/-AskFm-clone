3f47fedd265b71b581187aaa093c7ef248d8b2d2
22mahmoud <mahmouds1997@yahoo.com>
2018-02-09 06:56:51 +0200

    Set up routes w/ react-router-dom

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-const App = () => <h1> Hi </h1>;
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
+  </BrowserRouter>
+);
 export default App;
