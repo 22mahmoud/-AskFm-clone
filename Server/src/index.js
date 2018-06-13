60f2589e7051fe99e6d9c19e85106fdd05b1696d
22mahmoud <mahmouds1997@yahoo.com>
2018-06-13 22:03:25 +0200

    migrate to graphql yoga server

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,104 +1,27 @@
-import express from 'express';
-import { createServer } from 'http';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import { execute, subscribe } from 'graphql';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { GraphQLServer } from 'graphql-yoga';
 
 import path from 'path';
 
 import constants from './config/constants';
 import './config/db';
-import { decodeToken } from './services/auth';
-
-const app = express();
-require('dotenv').config();
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './graphql/schema')));
-
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './graphql/resolvers')));
 
-const schema = makeExecutableSchema({
+const server = new GraphQLServer({
   typeDefs,
   resolvers,
 });
 
-const auth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization;
-    if (token != null) {
-      const user = await decodeToken(token);
-      req.user = user;
-    } else {
-      req.user = null;
-    }
-    return next();
-  } catch (error) {
-    throw error;
-  }
+const options = {
+  cors: { origin: 'http://localhost:3000' },
+  port: constants.PORT,
+  endpoint: constants.GRAPHQL_PATH,
+  subscriptions: '/subscriptions',
+  playground: '/playground',
 };
 
-// middlewares
-app.use(bodyParser.json());
-app.use(cors('*'));
-app.use(auth);
-
-app.use(
-  '/graphiql',
-  bodyParser.json(),
-  graphiqlExpress({
-    endpointURL: constants.GRAPHQL_PATH,
-    subscriptionsEndpoint: `ws://localhost:${constants.PORT}/subscriptions`,
-  }),
-);
-
-app.use(
-  constants.GRAPHQL_PATH,
-  graphqlExpress(req => ({
-    schema,
-    context: {
-      user: req.user,
-    },
-  })),
-);
-
-const graphQlServer = createServer(app);
-
-graphQlServer.listen(constants.PORT, (err) => {
-  if (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-  } else {
-    // eslint-disable-next-line no-new
-    new SubscriptionServer(
-      {
-        execute,
-        subscribe,
-        schema,
-        // onConnect: (connectionParams, webSocket) =>
-        //   console.log(connectionParams, 'PARAMASSS') || { user: 'me' },
-        onConnect: async ({ token }) => {
-          if (token) {
-            try {
-              const user = await decodeToken(token);
-              if (user) {
-                return { user };
-              }
-            } catch (error) {
-              return { user: null };
-            }
-          }
-        },
-      },
-      {
-        server: graphQlServer,
-        path: '/subscriptions',
-      },
-    );
-    // eslint-disable-next-line no-console
-    console.log(`App listen on port: ${constants.PORT}`);
-  }
-});
+// eslint-disable-next-line no-console
+server.start(options, () =>
+  console.log('Server Running on http://localhost:3030/playground 🏃  🏃'));
